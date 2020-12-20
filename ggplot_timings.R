d40259e1e4818877bbbf1d2ca85f0d943e1700a2
Simon Pintarelli <simon.pintarelli@cscs.ch>
2020-12-20 21:36:19 +0100

    add showtext library to use computer modern font, size
    try to change aspect ratio

--- ggplot_timings.R
+++ ggplot_timings.R
@@ -1,9 +1,60 @@
+## first
+library(ggplot2)
+library(tidyverse)
+library(showtext)
+
+font_add("modern", "/usr/share/fonts/OTF/lmroman10-regular.otf")
+
+showtext_auto()
+
+
+data <- read.csv("timings.csv")
+data$nodes <- as.numeric(data$nodes)
+g1 <- ggplot(data, aes(x = nodes, y = time.max, colour = type, linetype = op)) +
+    geom_line() +
+    geom_point() +
+    scale_x_log10(breaks = c(128, 256, 512, 1024)) +
+    scale_y_log10() +
+    labs(linetype = "operation") +
+    labs(colour = "config") +
+    labs(y = "time [s]") +
+    labs(x = "#nodes") + theme(text = element_text(size=10, family="modern"))
+
+
+ggsave("loglog.pdf", g1, width=6, height=6)
+theme_set(
+    theme_light() + theme(legend.position = "bottom")
+)
+## sub
 library(ggplot2)
 library(dplyr)
 data <- read.csv("timings.csv")
-data$nodes  <- as.numeric(data$nodes)
-g1 <- ggplot(data, aes(x = nodes, y = time.max, colour = type, linetype = op)) +
-    geom_line() + geom_point() +
-    scale_x_log10() +
-    scale_y_log10()
-ggsave("loglog.pdf",  g1)
+data$nodes <- as.numeric(data$nodes)
+g1 <- ggplot(data, aes(x = nodes, y = time.max, colour = type)) +
+    geom_line() +
+    geom_point() +
+    scale_x_log10(breaks = c(128, 256, 512, 1024)) +
+    scale_y_log10() +
+    labs(linetype = "operation") +
+    labs(colour = "config") +
+    labs(y = "time [s]") +
+    labs(x = "#nodes") +
+  facet_wrap(~op, labeller = labeller(op = c(cp_gemm = "matrix multiplication", CP2K = "total time"))) +
+  theme(text  = element_text(size=10, family="modern"),  legend.title = element_blank())
+
+## +
+## theme(
+##     panel.background = element_rect(fill = "white", colour = "black"),
+##     panel.grid.major = element_line(
+##         size = 0.3, linetype = "dashed",
+##         colour = "gray"
+##     ), panel.grid.minor = element_line(
+##         size = 0.1, linetype = "dashed",
+##         colour = "gray"
+##     ),
+##   strip.background = element_rect(fill = "white"), # title background,
+##   legend.key = element_rect(fill = "white")
+## )
+
+
+ggsave("loglog-facets.pdf", g1, width=6, height=6,  units="in")
